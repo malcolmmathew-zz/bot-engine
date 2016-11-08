ba26b059693963d5b9fda472c3f3a66484a2531e
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-11-07 23:14:18 -0500

    class creation and logic for database config

--- engine.py
+++ engine.py
@@ -11,6 +11,7 @@
 		control flow
 	- we need an overarching unique identifier for a user => used when updating
 	record attributes
+	- we want keys/fields in JSON to be verbose
 
 
 	Example JSON
@@ -75,3 +76,59 @@
 		}
 	}
 """
+import json
+import os
+
+from pymongo import MongoClient
+
+
+class Engine: 
+	"""
+		Primary engine class for parsing JSON into application logic.
+	"""
+	def __init__(self, user_id, json_string):
+		"""
+			Constructor. Also handles database configuration.
+
+			Parameters
+			----------
+			user_id : {string}
+				alphanumeric identifier for user requiring engine
+
+			json_string : {JSON}
+				data obtained from multiple sources in JSON format.
+		"""
+		self.json_data = json.loads(json_string)
+		
+		self.client = MongoClient(os.environ["MONGO_IP"], 
+								  os.environ["MONGO_PORT"])
+
+		self.db = self.client[user_id]
+
+
+	def database_config(self):
+		"""
+			Database configuration parser. Currently used to create
+			database collections and insert placeholder records.
+
+			Only reason we create collections is for safety during record
+			insertions.
+		"""
+		db_config = self.json_data["database_configuration"]
+
+		for coll in db_config["collections"]:
+			self.db[coll].insert({"record": "placecholder"})
+
+
+if __name__ == '__main__':
+	# create dict, convert to JSON and then parse
+
+	test_data = {
+		"database_configuration" : {
+			"collections" : ["user", "transactions"]
+		}
+	}
+
+	json_data = json.dumps(test_data)
+
+	bot_engine = Engine("sunnithan95", json_data)
