02bda23c522d19ac1d4e4ae51d7cfe0b05a069b6
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-11-08 00:25:47 -0500

    base application logic and example json tweaks

--- engine.py
+++ engine.py
@@ -5,13 +5,21 @@
 	--------
 	- how do we correctly store chat-user responses; do we ask the engine-user
 	to define storage elements (e.g. db collections, objects)?
+	
 	- it makes most sense for message responses (i.e. user response to an
 	emitted message from a list) to be stored in a database
 		- the exception to this would be binary answers used to handle
 		control flow
-	- we need an overarching unique identifier for a user => used when updating
-	record attributes
+		- still haven't decided whether or not to store carousel responses 
+	
+	- we need an overarching unique identifier for a bot-user => used when 
+	updating record attributes
+	
 	- we want keys/fields in JSON to be verbose
+	
+	- I'm (Sid) aiming to keep the deployed application as thin as possible
+	with a minimal reliance on environment variables. Instead we hard-code said
+	variables and pass their values as template strings. 
 
 
 	Example JSON
@@ -30,19 +38,16 @@
 			"type": "carousel",
 			"buttons": [
 				{
-					"name": "log_x",
-					"target": <node_uid>,
-					"storage": <collection_name>
+					"name": "log_income",
+					"target": "income_amt_prompt"
 				},
 				{
 					"name": "log_y",
-					"target": <node_uid>,
-					"storage": <collection_name>
+					"target": <node_uid>
 				}, 
 				{
 					"name": "help",
-					"target": "help_message",
-					"storage": null
+					"target": "help_message"
 				}
 			]
 		},
@@ -70,8 +75,19 @@
 		"help_message": {
 			"type": "message_list",
 			"messages": [
-				"Generic help message. Something about a keyword that 
-				automagically generates the default carousel."
+				{
+					"message": "Generic help message."
+				}
+			]
+		},
+		"income_amount_prompt": {
+			"type": "message_list",
+			"messages": [
+				{
+					"message": "How much did you earn today?",
+					"expected_input": float,
+					"storage": "transactions.amount"
+				}
 			]
 		}
 	}
@@ -86,24 +102,38 @@ class Engine:
 	"""
 		Primary engine class for parsing JSON into application logic.
 	"""
-	def __init__(self, user_id, json_string):
+	def __init__(self, user_id, page_access_token, verify_token, json_string):
 		"""
-			Constructor. Also handles database configuration.
+			Constructor. Also handles database configuration and 
+			application logic instantiation.
 
 			Parameters
 			----------
 			user_id : {string}
 				alphanumeric identifier for user requiring engine
-
+			
+			page_access_token : {string}
+				alphanumeric token used to identify user's page; required for 
+				application logic
+		
+			verify_token : {string}
+				alphanumeric token for hub verification; required for 
+				application logic		
+		
 			json_string : {JSON}
 				data obtained from multiple sources in JSON format.
 		"""
 		self.json_data = json.loads(json_string)
-		
+		self.pat = page_access_token
+		self.vt = verify_token
+
+		# database config
 		self.client = MongoClient(os.environ["MONGO_IP"], 
 								  os.environ["MONGO_PORT"])
-
 		self.db = self.client[user_id]
+
+		# bot application config
+		self.application_logic = base_application_logic()
 
 
 	def database_config(self):
@@ -116,14 +146,107 @@ class Engine:
 		"""
 		db_config = self.json_data["database_configuration"]
 
+		db_config.append("state")
+
 		for coll in db_config["collections"]:
 			self.db[coll].insert({"record": "placecholder"})
+
+
+	def base_application_logic(self):
+		"""
+			Build the base for the bot's application logic. Handles app
+			instantiation, verification, and basic message sending.
+
+			For now we try our best to follow PEP8 standards.
+		"""
+		base_logic = """
+		import os
+		import json
+
+		import requests
+		from pymongo import MongoClient
+		from flask import Flask
+
+		from content import *
+
+		app = Flask(__name__)
+
+		# mongo constants
+		client = MongoClient({mongo_ip}, {mongo_port})
+		db = client[{user_id}]
+
+		# message sending helper
+		def send_message(recipient_id, message_data):
+			params = {
+				"access_token": {page_access_token}
+			}
+
+			headers = {
+				"Content-Type": "application/json"
+			}
+
+			data = json.dumps({
+				"recipient": {
+					"id": recipient_id
+				}, "message": message_data
+			})
+
+			r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+							  params=params, headers=headers, data=data)
+
+		@app.route("/", methods=["GET"])
+		def verify():
+			if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+				if not request.args.get("hub.verify_token") == {verify_token}:
+					return "Verificiation token mismatch", 403
+				return request.args["hub.challenge"], 200
+
+			return "Application Verified!", 200
+
+		@app.route("/", methods=["POST"])
+		{webhook_logic}
+
+		if __name__ == "__main__":
+			app.run(host="0.0.0.0", port=5000, debug=True)
+		"""
+
+		return base_logic
+
+
+	def webhook_logic(self):
+		"""
+			Method to string together all components of the webhook logic
+			(i.e. message, carousel, and quick reply handling).
+		"""
+
+		web_logic = """
+		"""
+
+		return web_logic
+
+	def content_creation(self):
+		"""
+			Primary method for bot content creation (i.e. separate file).
+		"""
+		return True
+
+	def process(self):
+		"""
+			Primary method to handle engine process. Calls member function in
+			particular order. The end results are application and content files
+			deployed and configured for the engine-user's facebook page.
+		"""
+
+		self.database_config()
+
+		return True
 
 
 if __name__ == '__main__':
 	# create dict, convert to JSON and then parse
 
 	test_data = {
+		"page_access_token": os.environ["PAGE_ACCESS_TOKEN"],
 		"database_configuration" : {
 			"collections" : ["user", "transactions"]
 		}
@@ -131,4 +254,7 @@ if __name__ == '__main__':
 
 	json_data = json.dumps(test_data)
 
-	bot_engine = Engine("sunnithan95", json_data)
+	bot_engine = Engine("sunnithan95", json_data["page_access_token"], 
+						json_data)
+
+	bot_engine.process()
