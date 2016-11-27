3faff6c2fb4998c5ea0d06ab50330e6358a0610f
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-11-27 02:10:44 -0500

    heroku application deployment; eventually need to merge these files

--- run.sh
+++ run.sh
@@ -5,11 +5,17 @@ source evars.sh
 user_id="$(python engine.py)"
 echo "USER ID: ${user_id}"
 
+# copy output directory to deployment sandbox
+cp -r ./output/$user_id ../bot-apps/$user_id
+
 # change working directory
-cd ./output/$user_id
+cd ../bot-apps/$user_id
+git init
 
 # application deployment - heroku
 heroku create
+git add .
+git commit -m "init commit - required"
 git push heroku master
-web_url="$(heroku info -s | grep web_url | cut -d= f2)"
+web_url="$(heroku info -s | grep web_url | cut -d= -f2)"
 echo "WEB URL: ${web_url}"
