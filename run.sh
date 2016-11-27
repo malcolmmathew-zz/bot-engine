221ada474d86f4f89c02b6c3a645c8d18bc4097c
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-11-27 12:11:08 -0500

    heroku env config and run/test file merge

--- run.sh
+++ run.sh
@@ -1,19 +1,31 @@
 # set appropriate environment variables
 source evars.sh
 
+# clean database - only for testing
+python database_setup.py
+
 # run engine and retrieve user id
-user_id="$(python engine.py)"
+engine_output="$(python engine.py)"
+output_arr=(${engine_output//,/ })
+user_id=${output_arr[0]}
+pat=${output_arr[1]}
+vt=${output_arr[2]}
 echo "USER ID: ${user_id}"
+echo "PAGE ACCESS TOKEN: ${pat}"
+echo "VERIFY TOKEN: ${vt}"
 
 # copy output directory to deployment sandbox
+rm -rf ../bot-apps/$user_id
 cp -r ./output/$user_id ../bot-apps/$user_id
 
 # change working directory
 cd ../bot-apps/$user_id
 git init
 
-# application deployment - heroku
+# application deployment
 heroku create
+heroku config:add PAGE_ACCESS_TOKEN=$pat
+heroku config:add VERIFY_TOKEN=$vt
 git add .
 git commit -m "init commit - required"
 git push heroku master
