fa23ff6a69921b1c798d9bddecebb57bec0006e1
Siddhanth Unnithan <sidunnithan@gmail.com>
2016-11-27 01:44:39 -0500

    run database clean-up script

--- test_engine.sh
+++ test_engine.sh
@@ -1,6 +1,9 @@
 # set appropriate environment variables
 source evars.sh
 
+# clean database - only for testing
+python database_setup.py
+
 # run engine and retrieve user id
 user_id="$(python engine.py)"
 echo "USER ID: ${user_id}"
